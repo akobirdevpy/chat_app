88c68bbf4ec8142f469623a3b52f2c5264b173fa
levsha <akobirsaidov023@gmail.com>
2025-10-24 07:10:24 +0500

    changed

--- chat_app/forms.py
+++ chat_app/forms.py
@@ -1,11 +1,1 @@
-from django import forms
 
-class Sending(forms.Form):
-    from_email = forms.EmailField()
-    to_email = forms.EmailField()
-    message = forms.TextField()
-
-class MessageForm(forms.ModelForm):
-    class Meta:
-        model = Message
-        fields = ('name', 'email', 'body')
