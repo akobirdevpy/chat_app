57502cfed89f4a775bcedf0e4ca34bfcfd70fcb8
levsha <akobirsaidov023@gmail.com>
2025-10-30 20:54:36 +0500

    deleted

--- chat_app/views.py
+++ chat_app/views.py
@@ -4,9 +4,4 @@ from django.http import JsonResponse
 def home_page(request):
     return render(request, 'index.html')
 
-def submit_form(request):
-    if request.method == "POST":
-        name = request.POST.get("name")
-        message = request.POST.get("message")
-        return JsonResponse({"message": f"Thanks {name}, your message was received!"})
-    return JsonResponse({"error": "Invalid request"}, status=400)
+
